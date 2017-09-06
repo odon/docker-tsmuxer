d8b66cc7f41559c820f71096ce8524f939f88b4b
odon <romain.veillerot@gmail.com>
2017-09-06 14:40:09 +0200

    add mkvtoolnix to container

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -2,3 +2,6 @@
 # NOTE: The $HOME variable is set only to have a default location when opening
 #       the file dialog window.
 exec env QT_X11_NO_MITSHM=1 HOME="/storage" /opt/tsmuxer/tsMuxerGUI
+
+/usr/local/bin/mkvtoolnix-gui --version
+exec /usr/local/bin/mkvtoolnix-gui
